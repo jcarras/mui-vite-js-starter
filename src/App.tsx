2402ef1a6401f6d25ffab7d3408e9f8eba8c865d
Joel C <joel-lseg@MacBook.broadband>
2023-11-24 06:28:01 +0000

    initial commit of project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+import ProTip from './ProTip';
+import Dashboard from './dashboard/Dashboard';
+
+export default function App() {
+  return (
+    <Dashboard />
+  );
+}
